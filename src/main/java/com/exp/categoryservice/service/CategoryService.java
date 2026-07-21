c9d04a4557235dfa57312b4aa4b5a8d8564bace9
shann606 <shann606@gmail.com>
2026-07-21 21:01:50 +0530

    Category and subcategory service is completed

--- src/main/java/com/exp/categoryservice/service/CategoryService.java
+++ src/main/java/com/exp/categoryservice/service/CategoryService.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -17,6 +19,7 @@ import com.exp.categoryservice.dto.CategoryReponse;
 import com.exp.categoryservice.dto.CategoryRequest;
 import com.exp.categoryservice.dto.SubCategoryRequest;
 import com.exp.categoryservice.dto.SubCategoryResponse;
+import com.exp.categoryservice.dto.UpdateSubCategory;
 import com.exp.categoryservice.dto.ViewSubCategory;
 import com.exp.categoryservice.entity.AddtionalInfo;
 import com.exp.categoryservice.entity.Category;
@@ -70,8 +73,13 @@ public class CategoryService {
 
 	private AddtionalInfo getAddtionalInfo(String createdBy) {
 
-		return AddtionalInfo.builder().createdby(createdBy)
-				.createdOn(OffsetDateTime.now(Clock.systemUTC())).build();
+		return AddtionalInfo.builder().createdby(createdBy).createdOn(OffsetDateTime.now(Clock.systemUTC())).build();
+
+	}
+
+	private AddtionalInfo getAddtionalInfoUpdate(String updatedBy) {
+
+		return AddtionalInfo.builder().updatedby(updatedBy).createdOn(OffsetDateTime.now(Clock.systemUTC())).build();
 
 	}
 
@@ -100,7 +108,7 @@ public class CategoryService {
 			createdT = dateConverter(toDate);
 		}
 		log.info("coming here to check");
-		return catRepo.categorySearch(name, status, createdF, createdT, PageRequest.of(pageNo, 2));
+		return catRepo.categorySearch(name, status, createdF, createdT, PageRequest.of(pageNo, 3));
 
 	}
 
@@ -114,7 +122,68 @@ public class CategoryService {
 
 	public Page<ViewSubCategory> viewSubCategories(UUID catId, int pageNo) {
 
-		return subCatRepo.viewSubCategories(catId, PageRequest.of(pageNo, 2));
+		return subCatRepo.viewSubCategories(catId, PageRequest.of(pageNo, 3));
+	}
+
+	@Transactional(timeout = 10)
+	public UpdateSubCategory updateSubCategory(UUID subid, UpdateSubCategory sub, String updatedBy) {
+
+		SubCategory subCat = subCatRepo.findById(subid)
+				.orElseThrow(() -> new CategoryNotFoundException("Sub-Category not found in the system."));
+
+		Optional.ofNullable(sub.name()).filter(StringUtils::hasText).ifPresent(subCat::setName);
+		Optional.ofNullable(sub.status()).ifPresent(subCat::setStatus);
+		Optional.ofNullable(sub.description()).filter(StringUtils::hasText).ifPresent(subCat::setDescription);
+
+		subCat.getInfo().setUpdatedby(updatedBy);
+		subCat.getInfo().setUpdatedOn(OffsetDateTime.now(Clock.systemUTC()));
+
+		subCat = subCatRepo.save(subCat);
+
+		return new UpdateSubCategory(subCat.getId(), subCat.getName(), subCat.getStatus(), subCat.getDescription());
+	}
+
+	public SubCategoryResponse getSubCategory(UUID subId) {
+
+		SubCategory sub = subCatRepo.findById(subId)
+				.orElseThrow(() -> new CategoryNotFoundException("Sub-Category not found in the system."));
+
+		return new SubCategoryResponse(sub.getId(), sub.getCatId(), sub.getName(), sub.getDescription(),
+				sub.getStatus());
+	}
+
+	@Transactional(timeout = 10)
+	public CategoryReponse updateCategory(UUID id, CategoryRequest req, String updatedBy) {
+
+		Category category = catRepo.findById(id)
+				.orElseThrow(() -> new CategoryNotFoundException("Category not found in the system"));
+
+		Optional.ofNullable(req.name()).filter(StringUtils::hasText).ifPresent(category::setName);
+		Optional.ofNullable(req.status()).ifPresent(category::setStatus);
+		Optional.ofNullable(req.description()).filter(StringUtils::hasText).ifPresent(category::setDescription);
+
+		category.getInfo().setUpdatedby(updatedBy);
+		category.getInfo().setUpdatedOn(OffsetDateTime.now(Clock.systemUTC()));
+
+		category = catRepo.save(category);
+
+		return new CategoryReponse(category.getId(), category.getName(), category.getDescription(),
+				category.getStatus(), category.getInfo().getUpdatedOn());
+	}
+
+	@Transactional
+	public Page<CategoryReponse> deleteCategory(UUID id) {
+
+		subCatRepo.deleteSubCategories(id);
+		catRepo.deleteById(id);
+
+		return categorySearch(null, null, null, null, 0);
+	}
+
+	@Transactional
+	public Page<ViewSubCategory> deleteSubCategory(UUID categoryId,UUID subCategoryId) {
+		subCatRepo.deleteById(subCategoryId);
+		return viewSubCategories(categoryId, 0);
 	}
 
 }
